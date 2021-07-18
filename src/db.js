caaffc0499aa4b623933084669b1375db34a4041
jakedavisOVO <jake.davis@ovoenergy.com>
2021-07-18 11:52:01 +0100

    adjusting db

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool
 const creds = require('../creds/creds')
 
 const pool = new Pool({
-    user: creds.user,
-    host: creds.host,
-    database: creds.database,
-    password: creds.password,
-    port: creds.port
+    user: "novjrmozfhfpuj",
+    host: "ec2-54-220-53-223.eu-west-1.compute.amazonaws.com",
+    database: "ddr8f9r4oj9g7b",
+    password: "055073e871236ffe9cae9de1af8ad3068d66c64ad6c0dfe6c813b1640239aa63",
+    port: 5432
 
 })
 
