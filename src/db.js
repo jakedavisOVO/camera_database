48ef0ac789074791a902816dddcc1fc03e7b28b4
jakedavisOVO <jake.davis@ovoenergy.com>
2021-07-19 08:42:22 +0100

    updating ssl

--- src/db.js
+++ src/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     host: "ec2-54-220-53-223.eu-west-1.compute.amazonaws.com",
     database: "ddr8f9r4oj9g7b",
     password: "055073e871236ffe9cae9de1af8ad3068d66c64ad6c0dfe6c813b1640239aa63",
-    port: 5432
+    port: 5432,
+    ssl: true
 
 })
 
